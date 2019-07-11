ee8bc25491d392167e8bdd5dcd8868278af2fe48
Brad Severance <me@bradseverance.com>
2019-07-11 10:26:26 -0400

    trying stuff

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ module.exports = app;
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
+    var message = 'It fucking works!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
